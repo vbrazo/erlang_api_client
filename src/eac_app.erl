44c241579e88ef13e3e47bf832f26d4f001137d0
Vitor Oliveira <vbrazo@gmail.com>
2021-08-26 15:15:24 -0700

    Add OTP application

---
+++ src/eac_app.erl
@@ -0,0 +1,24 @@
+%%%----------------------------------------------------------------------------
+%%% @author Vitor Oliveira <vbrazo@gmail.com>
+%%% @doc
+%%% API Erlang client - OTP application
+%%% @end
+%%%----------------------------------------------------------------------------
+-module(eac_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%%%============================================================================
+%%% Application callbacks
+%%%============================================================================
+-spec start(normal | {takeover, node()} | {failover, node()}, term()) ->
+               {ok, pid()} | {ok, pid(), term()} | {error, any()}.
+start(_StartType, _StartArgs) ->
+    eac_sup:start_link().
+
+-spec stop(term()) -> ok.
+stop(_State) ->
+    ok.
